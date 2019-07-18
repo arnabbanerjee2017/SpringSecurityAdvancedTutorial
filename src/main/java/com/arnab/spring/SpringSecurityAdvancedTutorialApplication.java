c7c76960df93266b6ec61bbe686067aca13d8729
Arnab Banerjee <arnab.ban09@gmail.com>
2019-07-18 23:45:29 +0530

    Initial commit

---
+++ src/main/java/com/arnab/spring/SpringSecurityAdvancedTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.arnab.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAdvancedTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityAdvancedTutorialApplication.class, args);
+	}
+
+}
